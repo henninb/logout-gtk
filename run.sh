ccc92f67f407e97f01b99c78edde2d251f417ad8
Brian Henning <henninb@msn.com>
2023-01-29 15:11:31 -0600

    wip

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-sudo pacman --noconfirm --needed -S gobject-introspection
-sudo dnf install -y gobject-introspection-devel
-sudo dnf install -y cairo-gobject-devel
-sudo dnf install -y atk-devel
-sudo dnf install -y gdk-pixbuf2-devel
-sudo dnf install -y pango-devel
-sudo dnf install -y gtk3-devel
+if command -v pacman; then
+  sudo pacman --noconfirm --needed -S gobject-introspection
+fi
+
+if command -v dnf; then
+  sudo dnf install -y gobject-introspection-devel
+  sudo dnf install -y cairo-gobject-devel
+  sudo dnf install -y atk-devel
+  sudo dnf install -y gdk-pixbuf2-devel
+  sudo dnf install -y pango-devel
+  sudo dnf install -y gtk3-devel
+fi
 mkdir -p src
 # stack run
 
