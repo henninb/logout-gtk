f47440b29d5f8006a0a5aa4edd5330d9455e935b
Brian Henning <henninb@msn.com>
2023-02-22 19:38:17 -0600

    wip

--- run.sh
+++ run.sh
@@ -2,20 +2,25 @@
 
 if command -v pacman; then
   sudo pacman --noconfirm --needed -S gobject-introspection
-fi
-
-sudo apt install -y gobject-introspection
-
-if command -v dnf; then
+elif command -v apt; then
+  sudo apt install -y gobject-introspection
+  sudo apt install -y libatk1.0-dev
+  sudo apt install -y libpango1.0-dev
+  sudo apt install -y libgdk-pixbuf-2.0-dev
+  sudo apt install -y libgirepository1.0-dev
+elif command -v dnf; then
   sudo dnf install -y gobject-introspection-devel
   sudo dnf install -y cairo-gobject-devel
   sudo dnf install -y atk-devel
   sudo dnf install -y gdk-pixbuf2-devel
   sudo dnf install -y pango-devel
   sudo dnf install -y gtk3-devel
+elif command -v xbps-install; then
+  sudo xbps-install -y gobject-introspection
+else
+  echo "$OS is not yet implemented."
+  exit 1
 fi
-
-sudo xbps-install -y gobject-introspection
 
 mkdir -p src
 # stack run
